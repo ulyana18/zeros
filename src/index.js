7f5021e4de81fce6289e7faff7cbf03b90c3f6a2
Ulyana Bychkovskaya <ubychkovskaya20@gmail.com>
2019-10-03 22:35:15 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let number='';
+  let symbol;
+  let five=0;
+  let two=0;
+
+  for(let i=0; i<expression.length; i++) {
+    symbol=expression[i];
+    switch(symbol) {
+      case '*': 
+        number='';
+        break;
+      case '!':
+        if(expression[i+1]==='!') {
+          for(let i=number; i>1; i-=2) {
+            let number = i;
+            calcZeros(+number);
+          }
+          i++;
+        }
+        else
+          for(let i=number; i>1; i--) {
+            let number = i;
+            calcZeros(+number);
+          }
+          break;
+      default: 
+        number+=symbol;
+        break;
+    }
+  }
+
+  if(five>two) return two;
+  else return five;
+
+
+  function calcZeros(number) {
+      while(number%5 == 0) {
+        number /= 5;
+        five++;
+      }
+      while(number%2 == 0) {
+        number /= 2;
+        two++;
+      }
+    
+  }
+    //return result;
 }
